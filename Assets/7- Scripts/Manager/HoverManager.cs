98fa046938f67e8641b0f446941432d73cd873d0
Archer01 <julienguenard01@gmail.com>
2023-06-01 11:58:54 +0200

    project reorganisation / fix bugs

---
+++ Assets/7- Scripts/Manager/HoverManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverManager : MonoBehaviour
+{
+    public static HoverManager instance;
+
+    private GameObject hoveredUnit;
+    private GameObject lastHoveredUnit;
+    private List<GameObject> hoveredUnitList = new List<GameObject>();
+
+    void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+    private void Update()
+    {
+        HoverNewUnit();
+    }
+
+    void HoverNewUnit()
+    {
+        lastHoveredUnit = hoveredUnit;
+        SetHoveredUnit(null);
+
+        if (hoveredUnitList.Count >= 1) HoverUnitAtCenter();
+
+     //   if (lastHoveredUnit != null && lastHoveredUnit != hoveredUnit) Unhover(lastHoveredUnit);
+
+     //   Hover();
+    }
+
+    public void HoverUnitAtCenter()
+    {
+        Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - new Vector3(0,0, Camera.main.ScreenToWorldPoint(Input.mousePosition).z);
+        float distanceNearest = 0;
+
+        foreach (GameObject obj in hoveredUnitList)
+        {
+            Vector3 objPos = obj.transform.position - new Vector3(0, 0, obj.transform.position.z);
+            float distance = (objPos - cursorPos).magnitude;
+
+            if (distanceNearest == 0) distanceNearest = distance;
+
+            if (hoveredUnit == null || distance < distanceNearest)
+            {
+                distanceNearest = distance;
+                SetHoveredUnit(obj);
+            }
+        }
+    }
+
+    void SetHoveredUnit(GameObject obj) { hoveredUnit = obj; }
+
+    public void HoverUnit(GameObject obj)
+    {
+        HoverNewUnit();
+        Hover();
+
+        if (hoveredUnitList.Contains(obj)) return;
+
+        hoveredUnitList.Add(obj);
+    }
+
+    public void UnhoverUnit(GameObject obj)
+    {
+        Unhover(obj);
+        hoveredUnitList.Remove(obj);
+    }
+
+    void Hover()
+    {
+        if (hoveredUnit == null) return;
+        if (SelectableManager.instance.GetSelectableUnitList().Contains(hoveredUnit)) return;
+
+        SelectableManager.instance.AddToSelectableUnitList(hoveredUnit);
+    }
+
+   void Unhover(GameObject obj)
+    {
+        SelectableManager.instance.RemoveToSelectableUnitList(obj);
+    }
+}
